2835d573c2e69178d5eb68fc938b98c196140811
Fernanda <fernandacarolineleite360@gmail.com>
2020-08-03 22:39:35 -0300

    Create teacher list page

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars1.githubusercontent.com/u/48728541?s=460&u=50a45fccecd761aebf1375e9e6236503e6782a13&v=4"
+          alt="Fernanda Leite"
+        />
+        <div>
+          <strong>Fernanda Leite</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonada por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+
+        <button type="button">
+          <img src={whatsappIcon} alt="WhatsApp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,40 @@
 import React from "react";
 
-// import { Container } from './styles';
+import "./styles.css";
+
+import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 const TeacherList: React.FC = () => {
-  return <div />;
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Esses são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matérias</label>
+            <input type="text" name="subject" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" name="week_day" id="week_day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" name="time" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  );
 };
 
 export default TeacherList;
